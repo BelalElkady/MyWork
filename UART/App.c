ef32433f439d5e42a5704fcca98419d184a2e017
Belal <Belal@Bela-PC>
2016-03-23 11:31:38 +0200

    UART driver

---
+++ UART/App.c
@@ -0,0 +1,21 @@
+/*
+ * App.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: Belal
+ */
+#include "types.h"
+#include "UART_private.h"
+#include "UART_interface.h"
+#include "avr/interrupt.h"
+
+ISR(USART_RXC_vect){
+
+}
+void main(void) {
+
+	while (1) {
+
+	}
+ return ;
+}
