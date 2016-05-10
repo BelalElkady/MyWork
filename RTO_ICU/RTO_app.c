f7c56246fd281eb2da1d9dadcd6d74f3f4bb7f3b
Belal <Belal@Bela-PC>
2016-05-10 15:16:02 +0300

    Input capture unit using Real time operating systems with a configurable System tick

---
+++ RTO_ICU/RTO_app.c
@@ -0,0 +1,128 @@
+/*
+ * RTO_app.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: Belal
+ */
+#include "types.h"
+#include "RTO_interface.h"
+#include "TIMER_interface.h"
+#include "DIO_interface.h"
+#include "DIO_private.h"
+#include "interrupt.h"
+
+volatile f32 Freq = 0;
+volatile f32 DutyCycle = 0;
+static u32 Local_u8Distance=0;
+
+void Task1(void) {
+	u8 Local_pinVal;
+	static u16 Local_Pve = 0;
+	static u16 Local_Nve = 0;
+	static u8 Local_EndFlag=0;
+	static u8 Local_NveFlag=0;
+	static u8 Local_PveFlag=0;
+	DIO_u8ReadPinVal(1, &Local_pinVal);
+
+	switch (Local_pinVal) {
+		case 1:
+			if (Local_PveFlag==1 && Local_NveFlag==1) {
+				Local_EndFlag=1;
+			}
+			Local_Pve++;
+			Local_PveFlag=1;
+			break;
+		case 0:
+			Local_Nve++;
+			Local_NveFlag=1;
+			break;
+		default:
+			break;
+	}
+
+	if (Local_EndFlag==1){
+		DutyCycle=(Local_Pve*100)/(Local_Nve+Local_Pve);
+		Freq=1/((Local_Nve+Local_Pve)*.012/*sys-tic measured by a percent of error*/);
+		DIO_u8WritePortVal(3,Freq);
+		Local_Pve=0;
+		Local_Nve=0;
+		Local_PveFlag=0;
+		Local_NveFlag=0;
+		Local_EndFlag=0;
+
+	}
+//	static u8 x = 0;
+//	x=!x;
+//	DIO_u8WritePinVal(16, x);
+
+
+
+	return;
+}
+
+void Task2(void) {
+	u8 Local_u8Value=0;
+
+	static u8 Local_u8Counter=0;
+	static u8 Flag=0;
+	DIO_u8ReadPinVal(16,&Local_u8Value);
+	switch (Local_u8Value){
+	case 1:
+		if (Flag==0){
+			Local_u8Counter++;
+		}
+		Flag=1;
+		break;
+
+	case 0 :
+		Flag=0;
+		break;
+
+	default:
+		break;
+
+	}
+
+	if (Local_u8Counter==10){
+		Local_u8Distance+=1;
+		Local_u8Counter=0;
+	}
+
+	return;
+}
+
+void Task3(void) {
+
+	if (Local_u8Distance>100){
+		DIO_u8WritePinVal(17,0);
+	}
+	else {
+		DIO_u8WritePinVal(17,1);
+	}
+
+	return;
+}
+void Task4(void) {
+
+
+
+
+	return;
+}
+
+int main(void) {
+
+	DIO_voidInit();
+	RTO_voidCreateTask(0, 1, Task2);
+	RTO_voidCreateTask(0, 4, Task3);
+	//RTO_voidCreateTask(0,4,Task3);
+	RTO_OSstart();
+
+	while (1) {
+
+	}
+
+	return 0;
+
+}
+
---
+++ RTO_ICU/TIMER_prog.c
@@ -0,0 +1,154 @@
+/*
+ * TIMER_prog.c
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: Belal
+ */
+#include "types.h"
+#include "TIMER_config.h"
+#include "TIMER_private.h"
+#include "TIMER_interface.h"
+#include "DIO_interface.h"
+#include "interrupt.h"
+
+
+static void (*PtrToOVISR)(void);
+static void (*PtrToCTCISR)(void);
+ volatile u8 Timer_u8Counter=0;
+
+
+extern void TIMER_voidInit(void) {
+
+	TCCR0 |= TIMER0_MODE | TIMER0_PRESCALER;
+	TIMSK |= TIMER0_OV_INTRRUPT |  TIMER0_CTC_INTRRUPT;
+
+	return;
+}
+
+extern u16 TIMER_u8SysTick(f32 Local_f32SetTick) {
+	u32  Local_Timer;
+	u16 NoOfOverFlow=0;
+
+#if TIMER0_PRESCALER==FOSC_0
+	Local_Timer=((Local_f32SetTick)*(F_CPU))/(1UL);
+	if (Local_Timer > 255 ){
+		NoOfOverFlow=((Local_f32SetTick)*(F_CPU))/(1UL*256UL);
+
+
+	}
+	else {
+		NoOfOverFlow=0;
+	    TIMER_voidSetCounter(255-Local_Timer);
+
+	}
+#elif TIMER0_PRESCALER==FOSC_8
+	Local_Timer=((Local_f32SetTick)*(F_CPU))/(8UL);
+	if (Local_Timer > 255 ){
+		NoOfOverFlow=((Local_f32SetTick)*(F_CPU))/(8UL*256UL);
+
+
+	}
+	else {
+		NoOfOverFlow=0;
+	    TIMER_voidSetCounter(255-Local_Timer);
+
+	}
+#elif TIMER0_PRESCALER==FOSC_64
+	Local_Timer=((Local_u16SetTick)*(F_CPU))/(64UL*256UL)
+#elif TIMER0_PRESCALER==FOSC_256
+	Local_Timer=((Local_u16SetTick)*(F_CPU))/(256UL*256UL);
+#elif TIMER0_PRESCALER==FOSC_1024
+
+	Local_Timer=((Local_f32SetTick)*(F_CPU))/(1024UL);
+	if (Local_Timer > 255 ){
+		NoOfOverFlow=((Local_f32SetTick)*(F_CPU))/(1024UL*256UL);
+
+
+	}
+	else {
+		NoOfOverFlow=0;
+	    TIMER_voidSetCounter(255-Local_Timer);
+
+	}
+
+
+
+#endif
+
+	return NoOfOverFlow;
+}
+
+extern void TIMER_voidSetCounter(u8 Copy_u8Value) {
+#if TIMER0_MODE==TIMER0_OVERFLOW
+
+	TCNT0 = Copy_u8Value;
+
+#elif TIMER0_MODE==TIMER0_CTC
+	OCR0=Copy_u8Value;
+	TCNT0=0;
+#endif
+
+	return;
+}
+
+extern void TIMER_voidCheckFlag(void) {
+
+#if TIMER0_MODE==TIMER0_OVERFLOW
+
+	while (!(TIFR & (1 << 0)))
+		;
+	TIFR |= (1 << 0);
+
+#elif TIMER0_MODE==TIMER0_CTC
+
+	while(!(TIFR &(1<<1)));
+	TIFR|=(1<<1);
+
+#endif
+}
+
+extern void TIMER_voidEnableOVInterrupt(void){
+	TIMSK|=(1<<0);
+	return;
+}
+extern void TIMER_voidEnableCTCInterrupt(void){
+	TIMSK|=(1<<1);
+	return;
+}
+extern void TIMER_voidDisableOVInterrupt(void){
+	TIMSK&=~(1<<0);
+	return ;
+}
+extern void TIMER_voidDisableCTCInterrupt(void){
+	TIMSK&=~(1<<1);
+	return ;
+}
+extern void TIMER_voidClearTimer(void){
+	Timer_u8Counter=0;
+}
+extern void TIMER_voidSetOVISR(void (*Copy_PtrToFun)(void)){
+
+	PtrToOVISR=Copy_PtrToFun;
+
+}
+extern void TIMER_voidSetCTCISR(void (*Copy_PtrToFun)(void)){
+
+	PtrToCTCISR=Copy_PtrToFun;
+}
+
+
+
+ISR(__vector_11){
+	Timer_u8Counter++;
+	PtrToOVISR();
+
+
+}
+
+
+ISR(__vector_10 ){
+	Timer_u8Counter++;
+	PtrToCTCISR();
+
+}
+
